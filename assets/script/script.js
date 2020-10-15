4c24d25560120ad933d4fa5698b4b92d33b81385
Bartek_makowski <mako19895@gmail.com>
2020-10-15 10:53:17 +0000

    added buttons variables and tested if they  work

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+const startButton = document.getElementById('button-start')
+const continueButton = document.getElementById('button-continue')
+const answer1 = document.getElementById('answer1')
+const answer2 = document.getElementById('answer2')
+const answer3 = document.getElementById('answer3')
+const answer4 = document.getElementById('answer4')
+const buttonRestart = document.getElementById('button-restart')
+
+answer2.addEventListener('click', test)
+answer3.addEventListener('click', test)
+buttonRestart.addEventListener('click', test)
+
+
+function test() {
+    console.log('works hahah!')
+}
