481c2c4a5dea30ae310142212eda882d1538ce72
Bartek_makowski <mako19895@gmail.com>
2020-10-20 22:54:02 +0000

    JS added 2 temp questions and question variables

---
+++ assets/script/script.js
@@ -0,0 +1,28 @@
+const question = document.getElementById('question-text');
+const choices = Array.from(document.getElementsByClassName('answer-text'));
+
+let currentQuestion = {};
+let awaitingQuestion = true;
+let score = 0;
+let questionNumber = 0;
+let avabQuestions = [];
+
+let questions = [
+    {
+        question: "who is your father ?",
+        choice1: "voldemort",
+        choice2: "Darth Vader",
+        choice3: "twoj stary",
+        choice4: "twoja stara",
+        answer: 2,
+    },
+
+    {
+        question: "why me ?",
+        choice1: "because",
+        choice2: "Yes",
+        choice3: "gdzie ty",
+        choice4: "co to sie stanelo ?",
+        answer: 4,
+    }
+]
